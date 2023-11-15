3096f534c8ff6178c7a2f81cc6e6d9fe7c3e4765
BoonBoonBoonBoon <Samacoitselliot@gmail.com>
2023-11-15 20:42:59 +0000

    .

--- Source/Perception/Private/BaseAI/ChaseAI/ChaseAI.cpp
+++ Source/Perception/Private/BaseAI/ChaseAI/ChaseAI.cpp
@@ -41,7 +41,6 @@ void AChaseAI::SetupStimulusSource()
 		StimuliSourcePredator->RegisterForSense(TSubclassOf<UAISense_Hearing>());
 		StimuliSourcePredator->RegisterWithPerceptionSystem();
 	}
-	
 }
 
 // Makes a Noise 
